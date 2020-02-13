578d029c563e00ab7d0c62578d5bf5156a84a9d4
dominicdaviescodes <codewithdom@gmail.com>
2020-02-13 15:49:35 +0000

    click now button is clicked hide subscriber page and show signup form

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // click button will hide subscriber page
 //  and  form will fade in
 
+document.querySelector('.banner-sub-here-btn').addEventListener('click', () => {
+	//  want to hide banner sub page
+	document.querySelector('.banner-sub-here').style.display = 'none';
+	// form will fade in
+	// cssText allows us to change several css properties
+	document.querySelector('.form-container').style.cssText = 'opacity: 1; visibility: visible;';
+	document.querySelector('.container').style.background = '#cc683c';
+});
+
 // click X to close will hide form and display subscriber page
